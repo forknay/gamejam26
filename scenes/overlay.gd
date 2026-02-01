b09f547629d65bf539974bf35bacfc6efe0c4f7f
Jessie Maeda-Yang <maeda.yang.jessie@gmail.com>
2026-02-01 01:16:11 -0500

    cwazy

---
+++ scenes/overlay.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func remove_overlay():
+	print("removing overlay")
+	set_mouse_filter(MOUSE_FILTER_IGNORE)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

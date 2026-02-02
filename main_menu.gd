b140c7bc29d4404eae0d604ede06f84a95a0ffd9
julien <julienyang12@gmail.com>
2026-02-01 19:20:12 -0500

    crt on menu

--- main_menu.gd
+++ main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	# Ensure the mouse is visible so they can click Play
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	$CRTLayer/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
 
 func _on_play_pressed():
 	# Start the game loop
